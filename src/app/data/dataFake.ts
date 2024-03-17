fd9e9aec41a7396185539846a7bf3c4885373f49
Luana Portezan <luanaportezan@gmail.com>
2024-03-16 23:19:27 -0300

    feat: updating my professional blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "title": "Sobre",
+    "description": "Desenvolvedora Freelancer com experiência desde 2022 em projetos de Web e App, com especialização em Front End. Minhas habilidades incluem HTML, CSS e JavaScript, sou proficiência em Angular e Ionic, que são meus frameworks preferidos. Ao longo da minha trajetória como freelancer, construí um histórico sólido com projetos que envolveram tecnologias como React.js, Node.js e MySQL, entre outras; - Experiência como Estagiária de Desenvolvimento em DevOps na TT&T Soluções e Informática. Minhas responsabilidades incluíam o planejamento de projetos, o desenvolvimento e os testes de aplicações de front-end em TypeScript com frameworks em Angular e Ionic, de back-end em Java com o framework em Spring Boot e JPA, e de banco de dados em MySQL. Além de ser a Scrum Master da equipe DevOps (estagiários) do período matutino; - Pós-graduada em Análise e Desenvolvimento de Programas pela Descomplica.",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
